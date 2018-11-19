1602b5c52aef16865efcf9e797ec48433628a0c6
Ammar Alnouirah <alnouirah@gmail.com>
2018-11-19 09:56:59 +0300

    adding overlayLoading section

--- js/plugin.js
+++ js/plugin.js
@@ -1,8 +1,20 @@
-$('.slider__item').on('mouseover', function(){
-    $('.slider__item').removeClass('active');
-    $(this).addClass('active');
-  });
-  $('.slider__item').on('mouseout', function(){
-    $('.slider__item').removeClass('active');
-    $(this).addClass('');
-  });
+
+
+
+  $( document ).ready(function() {
+    
+    setTimeout(function(){
+      $('.overlayLoading').fadeOut(800,function(){
+        $(this).remove();
+      })
+    }, 1000);
+    
+    $('.slider__item').on('mouseover', function(){
+      $('.slider__item').removeClass('active');
+      $(this).addClass('active');
+    });
+    $('.slider__item').on('mouseout', function(){
+      $('.slider__item').removeClass('active');
+      $(this).addClass('');
+    });
+});
